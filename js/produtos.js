f982d31226d58f628277d9e1ce5f79e75236adc3
Fernando Junior <fernandojunior@MacBook-Air-de-Fernando.local>
2023-02-11 13:18:21 -0300

    Ajustado JS da pagina de produtos

--- js/produtos.js
+++ js/produtos.js
@@ -3,6 +3,7 @@ const token = "e82b7ce9aefe9ea912d47c59cf8875";
 const url = new URL(window.location.href);
 const categoria = url.searchParams.get("categoria");
 const marca = url.searchParams.get("marca");
+const produto = url.searchParams.get("produto");
 
 const queryProdutosCategoria = `
       {
@@ -31,6 +32,27 @@ const queryProdutosCategoria = `
       }
   `;
 
+const queryProduto = `
+      {
+        product(filter: {id: {eq: ${produto}}}) {
+          id, preco, nome, descricao, ativo, 
+          brand {
+            id, nome
+          },
+          category {
+            id, nome
+          },
+          productHighlight {
+            id, nome
+          },
+          imagens {
+            id, url
+          },
+          _createdAt
+        }
+      }
+`;
+
 const maisPesquisados = document.getElementById("maisPesquisados");
 const ultimosAnuncios = document.getElementById("ultimosAnuncios");
 const tituloCardsProdutos = document.getElementById("tituloCardsProdutos");
@@ -52,8 +74,14 @@ function buscarProdutos(query) {
   })
     .then((res) => res.json())
     .then((res) => {
-      mostrarTituloProdutos(res.data.category);
-      mostrarListaProdutos(res.data.allProducts);
+      console.log(res);
+      if (categoria) {
+        mostrarTituloProdutos(res.data.category);
+        mostrarListaProdutos(res.data.allProducts);
+      }
+      if (produto) {
+        mostrarProduto(res.data.product);
+      }
     })
     .catch((error) => {
       console.error(error);
@@ -61,9 +89,10 @@ function buscarProdutos(query) {
 }
 
 if (categoria) buscarProdutos(queryProdutosCategoria);
+if (produto) buscarProdutos(queryProduto);
 
 function mostrarTituloProdutos(categoriaOuMarca) {
-  document.title = categoriaOuMarca.nome;
+  alterarTituloDaPagina(categoriaOuMarca.nome);
 
   tituloCardsProdutos.innerHTML = `
       <img
@@ -81,11 +110,77 @@ function mostrarListaProdutos(produtos) {
   });
 }
 
+function mostrarProduto(produto) {
+  alterarTituloDaPagina(produto.nome);
+  const containerProduto = document.getElementById("containerProduto");
+  const dadosProduto = document.createElement("div");
+  dadosProduto.className = "fundo-wrapper";
+  dadosProduto.innerHTML = `
+          <div class="conteudo">
+            <div class="imagens">
+              <div class="galeria-imagens" id="galeriaImagens"></div>
+
+              <div class="imagem-principal">
+                <img
+                  id="imagemPrincipal"
+                  src="${produto.imagens[0].url}"
+                  alt="${produto.nome}"
+                />
+              </div>
+            </div>
+
+            <div class="informacao">
+              <h3>
+                ${produto.nome}
+              </h3>
+              <div class="preco">
+                <span>${formatarValorMoeda(produto.preco)}</span>
+                <span>ou em até 3x de ${formatarValorMoeda(
+                  produto.preco / 3
+                )} sem juros</span>
+              </div>
+
+              <a href="#" id="forma-pagamento">ver mais opções de pagamento</a>
+
+              <button class="comprar" id="comprar" type="button">
+                COMPRAR
+              </button>
+
+              <div class="calcular-frete">
+                <span>Consultar frete e prazo de entrega</span>
+                <div class="cep">
+                  <input
+                    type="number"
+                    name="cep"
+                    id="cep"
+                    placeholder="Inserir CEP"
+                  />
+                  <button class="button-cep" id="ok" type="button">OK</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <hr />
+
+          <div class="descricao-produto">
+            <h3>Descrição Produto</h3>
+            <span>
+              ${formatarDescricaoProduto(produto.descricao)}
+            </span> 
+          </div>
+        `;
+  containerProduto.appendChild(dadosProduto);
+  criarGaleriaImagensProduto(produto);
+}
+
 function criarCardProduto(produto) {
   const card = document.createElement("div");
   card.className = "card-produto-wrapper";
   card.innerHTML = `
-          <a href="#" class="conteudo-card-produto">
+          <a href="produto.html?produto=${
+            produto.id
+          }" class="conteudo-card-produto">
               <div class="img-card-produto">
                   <img src="${produto.imagens[0].url}" alt="${produto.nome}">
               </div>
@@ -99,7 +194,37 @@ function criarCardProduto(produto) {
   return card;
 }
 
+function criarGaleriaImagensProduto(produto) {
+  const galeriaImagens = document.getElementById("galeriaImagens");
+
+  produto.imagens.forEach((imagem) => {
+    const img = document.createElement("img");
+    img.src = imagem.url;
+    img.alt = produto.nome;
+
+    img.addEventListener("mouseover", () => trocarImagemProduto(imagem.url));
+
+    galeriaImagens.appendChild(img);
+  });
+}
+
+function trocarImagemProduto(url) {
+  const imagemPrincipal = document.getElementById("imagemPrincipal");
+  imagemPrincipal.src = url;
+}
+
+function formatarDescricaoProduto(descricao) {
+  // const descricaoFormatada = descricao.replace(/\\n/g, "<br>");
+  console.log(JSON.stringify(descricao).includes("/a/"));
+
+  return descricao;
+}
+
 // FUNÇÕES DE USO GERAL
 function formatarValorMoeda(valor) {
   return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+}
+
+function alterarTituloDaPagina(title) {
+  document.title = title;
 }
