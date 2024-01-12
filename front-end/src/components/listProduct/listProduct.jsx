1c20b1d386df409abb7df8a6835390e0d2d53af4
Fernando Junior <fernandojunior@MacBook-Air-de-Fernando.local>
2024-01-12 11:35:13 -0300

    dados atualizados

--- front-end/src/components/listProduct/listProduct.jsx
+++ front-end/src/components/listProduct/listProduct.jsx
@@ -1,7 +1,6 @@
 import CardProduct from "../cardProduct/cardProduct";
 
 function ListProduct({ produtos }) {
-
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 text-center bg-white p-4">
       {produtos?.map((produto) => (
